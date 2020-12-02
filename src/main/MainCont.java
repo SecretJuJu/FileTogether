83cf89058b5881134f5d476d09e4859e1d0efab9
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2020-12-03 07:32:16 +0900

    Update MainCont.java

--- src/main/MainCont.java
+++ src/main/MainCont.java
@@ -1,23 +1,34 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import client.Client;
+
 import server.Server;
 
 public class MainCont {
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("input : ");
-		String str = sc.next();
-		
-		if (str.equals("s")) {
-			Server s = new Server();
-			s.server();
-		}else {
-			Client c = new Client();
-			c.client();
-		}
-	}
+    public static void main(String[] args) {
+        ArrayList<String> serverList = new ArrayList<String>();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input : ");
+        String str = sc.next();
+
+        if (str.equals("s")) {
+            Server s = new Server();
+            s.makeServer();
+        }else {
+            Client c = new Client();
+//          serverList = c.scan();
+            ArrayList<Client.InterfaceData> interfaces = c.getInterfaces();
+            interfaces.forEach(item -> {
+                System.out.println("============");
+                System.out.println(item.getNicName());
+                System.out.println(item.getIpAddress());
+            });
+            
+//            System.out.println(serverList);
+        }
+    }
 }
